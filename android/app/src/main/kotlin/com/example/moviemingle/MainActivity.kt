8773fbddb881044b53b8820b2c957c9641c8d4b9
Pulkit <pulkitkhatter13@gmail.com>
2024-11-29 22:27:49 +0530

    m

---
+++ android/app/src/main/kotlin/com/example/moviemingle/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.moviemingle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,317 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Simulate a splash screen for 3 seconds and then navigate to the HomeScreen
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset(
+            'assets/images/movie_splash.png'), // Add your splash image here
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> movies = [];
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMovies();
+  }
+
+  // Fetching movies from the API
+  Future<void> fetchMovies() async {
+    try {
+      final response = await http
+          .get(Uri.parse('https://api.tvmaze.com/search/shows?q=all'));
+
+      if (response.statusCode == 200) {
+        final List fetchedMovies = json.decode(response.body);
+        setState(() {
+          movies = List<Map<String, dynamic>>.from(fetchedMovies);
+        });
+      } else {
+        throw Exception('Failed to load movies');
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text('Failed to load movies. Please try again later.')),
+      );
+    }
+  }
+
+  // Handle Bottom Navigation Bar item selection
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  // Navigating to SearchScreen or HomeScreen based on the selected index
+  Widget _getScreen() {
+    if (_currentIndex == 0) {
+      return HomeScreenBody(movies: movies);
+    } else {
+      return SearchScreen();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('MovieMingle'),
+      ),
+      body: _getScreen(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+      ),
+    );
+  }
+}
+
+class HomeScreenBody extends StatelessWidget {
+  final List<Map<String, dynamic>> movies;
+
+  const HomeScreenBody({Key? key, required this.movies}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return movies.isEmpty
+        ? Center(child: CircularProgressIndicator())
+        : GridView.builder(
+            padding: EdgeInsets.all(10),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 0.7,
+            ),
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final movie = movies[index]['show'];
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailsScreen(movie: movie),
+                    ),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                      image: movie['image'] != null &&
+                              movie['image']['medium'] != null
+                          ? NetworkImage(movie['image']['medium'])
+                          : AssetImage('assets/images/placeholder.png')
+                              as ImageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      color: Colors.black.withOpacity(0.6),
+                      child: Text(
+                        movie['name'],
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+  }
+}
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController _controller = TextEditingController();
+  List searchResults = [];
+
+  // Fetch search results
+  Future<void> searchMovies(String query) async {
+    if (query.isEmpty) return;
+
+    try {
+      final response = await http
+          .get(Uri.parse('https://api.tvmaze.com/search/shows?q=$query'));
+
+      if (response.statusCode == 200) {
+        final List fetchedMovies = json.decode(response.body);
+        setState(() {
+          searchResults = fetchedMovies;
+        });
+      } else {
+        throw Exception('Failed to search movies');
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text('Failed to search movies. Please try again later.')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search Movies'),
+        backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Search Movies',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              onChanged: searchMovies,
+            ),
+            Expanded(
+              child: searchResults.isEmpty
+                  ? Center(
+                      child: Text('No results found. Try a different query.'))
+                  : ListView.builder(
+                      itemCount: searchResults.length,
+                      itemBuilder: (context, index) {
+                        final movie = searchResults[index]['show'];
+                        return ListTile(
+                          contentPadding: EdgeInsets.all(10),
+                          leading: movie['image'] != null
+                              ? Image.network(movie['image']['medium'])
+                              : Icon(Icons.image_not_supported),
+                          title: Text(movie['name']),
+                          subtitle: Text(
+                            movie['summary']
+                                    ?.replaceAll(RegExp(r'<[^>]*>'), '') ??
+                                'No summary available',
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    DetailsScreen(movie: movie),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  final dynamic movie;
+
+  const DetailsScreen({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movie['name']),
+        backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            movie['image'] != null
+                ? Image.network(movie['image']['original'])
+                : SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: Icon(Icons.image_not_supported),
+                  ),
+            SizedBox(height: 16),
+            Text(
+              movie['name'],
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              movie['summary']?.replaceAll(RegExp(r'<[^>]*>'), '') ??
+                  'No summary available',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
